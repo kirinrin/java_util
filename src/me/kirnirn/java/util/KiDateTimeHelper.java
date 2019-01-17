71538b472b12d7c6b84443f0b52b60d41ab000d1
Kirinrin <will.lain@gmail.com>
2019-01-17 16:06:39 +0800

    添加数据库转型工具和日期工具

--- src/me/kirnirn/java/util/KiDateTimeHelper.java
+++ src/me/kirnirn/java/util/KiDateTimeHelper.java
@@ -1,5 +1,6 @@
 package me.kirnirn.java.util;
 
+import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -9,10 +10,12 @@ import java.time.format.DateTimeFormatter;
 
 public class KiDateTimeHelper {
 
-	public static  DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	public static DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
 	/**
 	 * Convert Instant to LocalDateTime by system default zone
+	 * 
 	 * @param instant
 	 * @return
 	 */
@@ -21,8 +24,11 @@ public class KiDateTimeHelper {
 		LocalDateTime ldt = LocalDateTime.ofInstant(instant, zoneId);
 		return ldt;
 	}
+
+
 	/**
 	 * Convert Instant to LocalDateTime by time zone
+	 * 
 	 * @param instant
 	 * @return
 	 */
@@ -33,31 +39,34 @@ public class KiDateTimeHelper {
 
 	/**
 	 * 计算时间差（秒），如果某个参数=0， 返回 0
+	 * 
 	 * @param start
 	 * @param end
 	 * @return
 	 */
 	public static Long durationBySecond(LocalDateTime start, LocalDateTime end) {
-		if(start == null || end == null) {
+		if (start == null || end == null) {
 			return 0L;
-		}else {
-			return Duration.between(start, end).getSeconds();			
+		} else {
+			return Duration.between(start, end).getSeconds();
 		}
 	}
+
 	/**
 	 * 计算时间差（秒），如果某个参数=0， 返回 0, 三个比较，使用后两个中的前面的一个。
+	 * 
 	 * @param start
 	 * @param end
 	 * @return
 	 */
 	public static Long durationBySecond(LocalDateTime start, LocalDateTime mid, LocalDateTime end) {
-		if(start == null || ( mid == null && end == null)) {
+		if (start == null || (mid == null && end == null)) {
 			return 0L;
-		}else if(mid == null){
-			return Duration.between(start, end).getSeconds();			
-		}else if(end == null) {
+		} else if (mid == null) {
+			return Duration.between(start, end).getSeconds();
+		} else if (end == null) {
 			return Duration.between(start, mid).getSeconds();
-		}else {
+		} else {
 			return 0L;
 		}
 	}
